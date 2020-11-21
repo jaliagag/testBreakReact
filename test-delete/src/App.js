c5999442f0460f8685c7beaa1c2110a5edb2cae6
jaliagag <jmfaliaga@gmail.com>
2020-11-20 21:33:39 -0300

    working on a form using react

--- test-delete/src/App.js
+++ test-delete/src/App.js
@@ -6,6 +6,23 @@ function App() {
   const [hola, setHola] = useState(20);
   const [adios, setAdios] = useState(30);
   const [textito, setTextito] = useState('Write your shit here')
+  const [usuario, setUsuario] = useState({
+    nombre: ''
+    ,apellido: ''
+  })
+
+  const nino = (e) => {
+    setUsuario({
+      ...usuario
+      , [e.target.name] : e.target.value
+    });
+  }
+
+  const enviarDatos = (e) => {
+    e.preventDefault();
+    console.log('enviando datos...' + usuario.nombre + ' ' + usuario.apellido);
+  }
+  
   function change(){
     if (hola === 20) {
       setHola(30);
@@ -15,8 +32,10 @@ function App() {
       setAdios(30);
     }
   }
+
   function asdf(){
   }
+
   return (
     <>
       <div className="">
@@ -25,6 +44,7 @@ function App() {
 	<h1>{adios}</h1>
       </div>
       <div>
+    <p>###########################</p>
 	<input
 	  type='text'
 	  placeholder='your sheit'
@@ -36,6 +56,19 @@ function App() {
 	  <span>{textito}</span>
 	</div>
       </div>
+    <p>###########################</p>
+    <form className="" onSubmit={enviarDatos}>
+      <input type="text" placeholder="Nombre" onChange={nino} name="nombre"></input><br/>
+    {/*      //<input type="text" placeholder="Nombre" onChange={nino} name="nombre"></input><br/>*/}
+      <input type="text" placeholder="Apellido" onChange={nino} name="apellido"></input><br />
+    {/*      //<input type="text" placeholder="Apellido" onChange={nino} name="apellido"></input><br />*/}
+      <button type="submit" className="">Crear</button>
+    </form>
+    {/*<ul>
+      <li>{usuario.nombre}</li>
+      <li>{usuario.apellido}</li>
+    </ul>*/}
+
     </>
   );
 }
